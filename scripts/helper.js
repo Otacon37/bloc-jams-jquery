f7ae9ca9bdc26fb66680b2cb8a8a88bc12439f0b
Dan Balliet <dandibiasio37@gmail.com>
2017-11-18 14:49:14 -0500

    Helper.js at 95% functionality

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,8 +1,9 @@
 class Helper {
-  playPauseAndUpdate (song) {
+    playPauseAndUpdate (song) {
+
     player.playPause(song);
-    const duration = player.getDuration();
-      $('#time-control .total-time').text(duration);
+    const totalDuration = player.getDuration();
+      $('#time-control .total-time').text(totalDuration);
     }
   }
 
