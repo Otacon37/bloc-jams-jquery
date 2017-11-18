908a62ea3ccb3a3e8e4720e5955c90d383f512f6
Dan Balliet <dandibiasio37@gmail.com>
2017-11-18 12:42:04 -0500

    Fix helper.js to show total time when song is clicked

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,6 +1,5 @@
 class Helper {
   playPauseAndUpdate (song) {
-    if (player.playState !== 'playing') { return; }
     player.playPause(song);
     const duration = player.getDuration();
       $('#time-control .total-time').text(duration);
