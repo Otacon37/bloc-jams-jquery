868988b4860da944ffe15f42345c5a45c599d4c6
Dan Balliet <dandibiasio37@gmail.com>
2017-11-18 12:28:28 -0500

    add Helper.js

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,12 +1,10 @@
 class Helper {
   playPauseAndUpdate (song) {
     if (player.playState !== 'playing') { return; }
-    player.playPause(this);
+    player.playPause(song);
     const duration = player.getDuration();
       $('#time-control .total-time').text(duration);
     }
   }
-
-}
 
 const helper = new Helper();
